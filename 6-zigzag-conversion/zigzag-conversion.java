cbd47b63ffeec6adcb8673ef93053bcc223bb256
Hamzeh Sayeh <116796653+HamzehSayeh@users.noreply.github.com>
2024-07-20 23:58:03 +0300

    Time: 53 ms (6.56%) | Memory: 45.9 MB (9.93%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,25 +1,7 @@
 class Solution {
     public String convert(String s, int numRows) {
         int characterCount = 0;
-        int length = s.length();
-
-        if (numRows < 2) {
-            return s;
-        }
-
-        int cycleLength = numRows + numRows - 2;
-        int fullCycles = length / cycleLength;
-        int remainder = length % cycleLength;
-
-        int numColumns = fullCycles * (numRows - 1);
-        if (remainder > 0) {
-            if (remainder <= numRows) {
-                numColumns += 1;
-            } else {
-                numColumns += 1 + (remainder - numRows);
-            }
-        }
-        char[][] zigzagString = new char[numRows][numColumns];
+        char[][] zigzagString = new char[numRows][500];
         int row = 0;
         int column = 0;
         StringBuilder result = new StringBuilder();
@@ -28,13 +10,12 @@ class Solution {
             return s;
         }
 
-        while (characterCount < length) {
-            for (; row < numRows && characterCount < length; row++, characterCount++) {
+        while (characterCount < s.length()) {
+            for (; row < numRows && characterCount < s.length(); row++, characterCount++) {
                 zigzagString[row][column] = s.charAt(characterCount);
             }
-            row -= 2;
             column++;
-            for (; row >= 0 && characterCount < length; row--, column++, characterCount++) {
+            for (row = numRows - 2; row >= 0 && characterCount < s.length(); row--, column++, characterCount++) {
                 zigzagString[row][column] = s.charAt(characterCount);
             }
             row += 2;
@@ -42,12 +23,13 @@ class Solution {
         }
 
         characterCount = 0;
-        for (int i = 0; i < numRows; i++)
+        for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < zigzagString[i].length; j++, characterCount++) {
-                if (zigzagString[i][j] != '\0')
+                if (zigzagString[i][j] != '\0') {
                     result.append(zigzagString[i][j]);
+                }
             }
-
+        }
         return result.toString();
     }
 }
