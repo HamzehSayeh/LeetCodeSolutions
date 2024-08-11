883219f0e5636e23abb15b219b99769230a128c6
Hamzeh Sayeh <116796653+HamzehSayeh@users.noreply.github.com>
2024-08-11 17:19:13 +0300

    Time: 0 ms (100.00%) | Memory: 40.6 MB (9.67%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -4,9 +4,8 @@ class Solution {
             return 1;
         int current = 1, previous = 1;
         for (int i = 0; i < n - 1; i++) {
-            int temp = current;
             current = previous + current;
-            previous = temp;
+            previous = current-previous;
         }
         return current;
     }
